f2e08afdbcaca1416be258ff1ac1b79e5a45dadd
bartosz.kutkowski <bartosz.kutkowski@pansa.pl>
2021-09-16 15:00:37 +0200

    turnOffScreenshots

--- src/main/java/base/TestConfiguration.java
+++ src/main/java/base/TestConfiguration.java
@@ -2,8 +2,7 @@ package base;
 
 import lombok.NoArgsConstructor;
 
-import static com.codeborne.selenide.Configuration.headless;
-import static com.codeborne.selenide.Configuration.timeout;
+import static com.codeborne.selenide.Configuration.*;
 import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor(access = PRIVATE)
@@ -12,5 +11,6 @@ public class TestConfiguration {
     public static void setBrowserConfiguration() {
         headless = false;
         timeout = 10_000;
+        screenshots = false;
     }
 }
